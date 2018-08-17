5312c6699d45e1fa2d36a1acfb56c7a2310a55b2
Wes Hampson <thehambone93@gmail.com>
2018-08-17 00:20:11 -0500

    Add StringExtensions

---
+++ Gta3CarGenEditor/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace WHampson.Gta3CarGenEditor.Extensions
+{
+    /// <summary>
+    /// Extensions to the <see cref="string"/> class.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Removes all NUL characters from a string.
+        /// </summary>
+        /// <param name="s">The string to strip of NUL characters.</param>
+        /// <returns>The stripped string.</returns>
+        public static string StripNull(this string s)
+        {
+            return s.Replace("\0", "");
+        }
+    }
+}
