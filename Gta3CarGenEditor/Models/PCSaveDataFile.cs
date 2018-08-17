c9c18d1d054feeb1eb12209cd0a56085a795f978
Wes Hampson <thehambone93@gmail.com>
2018-08-16 20:16:06 -0500

    Use new Read/Write functions for (de)serialization

--- Gta3CarGenEditor/Models/PCSaveDataFile.cs
+++ Gta3CarGenEditor/Models/PCSaveDataFile.cs
@@ -10,133 +10,36 @@ namespace WHampson.Gta3CarGenEditor.Models
 
         public PCSaveDataFile()
             : base(GamePlatform.PC)
-        { }
+        {
+            m_simpleVars.Data = new byte[SizeOfSimpleVars];
+        }
 
         protected override long DeserializeObject(Stream stream)
         {
             long start = stream.Position;
             using (BinaryReader r = new BinaryReader(stream, Encoding.Default, true)) {
-                int bytesRead;
-                int blockSize;
-                int totalSize;
-                int totalBytesRead;
-
-                // Read SimpleVars and Scripts block size
-                totalBytesRead = 0;
-                totalSize = r.ReadInt32();
-
-                // Read SimpleVars
-                m_simpleVars = r.ReadBytes(SizeOfSimpleVars);
-                totalBytesRead += m_simpleVars.Length;
-
-                // Read Scripts
-                blockSize = r.ReadInt32();
-                m_scripts = r.ReadBytes(blockSize);
-
-                totalBytesRead += m_scripts.Length + 4;
-                if (totalBytesRead != totalSize) {
-                    string msg = string.Format("{0}: {1}",
-                        nameof(PCSaveDataFile), Resources.IncorrectNumberOfBytesDecodedMessage);
-                    throw new InvalidDataException(msg);
-                }
-
-                // Read PlayerPeds
-                blockSize = r.ReadInt32();
-                m_playerPeds = r.ReadBytes(blockSize);
-
-                // Read Garages
-                blockSize = r.ReadInt32();
-                m_garages = r.ReadBytes(blockSize);
-
-                // Read Vehicles
-                blockSize = r.ReadInt32();
-                m_vehicles = r.ReadBytes(blockSize);
-
-                // Read Objects
-                blockSize = r.ReadInt32();
-                m_objects = r.ReadBytes(blockSize);
-
-                // Read PathFind
-                blockSize = r.ReadInt32();
-                m_pathFind = r.ReadBytes(blockSize);
-
-                // Read Cranes
-                blockSize = r.ReadInt32();
-                m_cranes = r.ReadBytes(blockSize);
-
-                // Read Pickups
-                blockSize = r.ReadInt32();
-                m_pickups = r.ReadBytes(blockSize);
-
-                // Read PhoneInfo
-                blockSize = r.ReadInt32();
-                m_phoneInfo = r.ReadBytes(blockSize);
-
-                // Read Restarts
-                blockSize = r.ReadInt32();
-                m_restarts = r.ReadBytes(blockSize);
-
-                // Read Radar
-                blockSize = r.ReadInt32();
-                m_radar = r.ReadBytes(blockSize);
-
-                // Read Zones
-                blockSize = r.ReadInt32();
-                m_zones = r.ReadBytes(blockSize);
-
-                // Read Gangs
-                blockSize = r.ReadInt32();
-                m_gangs = r.ReadBytes(blockSize);
-
-                // Read CarGenerators
-                totalSize = r.ReadInt32();
-                blockSize = r.ReadInt32();
-
-                bytesRead = (int) Deserialize(stream, out m_carGenerators);
-                if (bytesRead != blockSize) {
-                    string msg = string.Format("{0}: {1}",
-                        nameof(PCSaveDataFile), Resources.IncorrectNumberOfBytesDecodedMessage);
-                    throw new InvalidDataException(msg);
-                }
-
-                totalBytesRead = bytesRead + 4;
-                if (totalBytesRead != totalSize) {
-                    string msg = string.Format("{0}: {1}",
-                        nameof(PCSaveDataFile), Resources.IncorrectNumberOfBytesDecodedMessage);
-                    throw new InvalidDataException(msg);
-                }
-
-                // Read Particles
-                blockSize = r.ReadInt32();
-                m_particles = r.ReadBytes(blockSize);
-
-                // Read AudioScriptObjects
-                blockSize = r.ReadInt32();
-                m_audioScriptObjects = r.ReadBytes(blockSize);
-
-                // Read PlayerInfo
-                blockSize = r.ReadInt32();
-                m_playerInfo = r.ReadBytes(blockSize);
-
-                // Read Stats
-                blockSize = r.ReadInt32();
-                m_stats = r.ReadBytes(blockSize);
-
-                // Read Streaming
-                blockSize = r.ReadInt32();
-                m_streaming = r.ReadBytes(blockSize);
-
-                // Read PedTypes
-                blockSize = r.ReadInt32();
-                m_pedTypes = r.ReadBytes(blockSize);
-
-                // Read Padding0
-                blockSize = r.ReadInt32();
-                m_padding0 = r.ReadBytes(blockSize);
-
-                // Read Padding1
-                blockSize = r.ReadInt32();
-                m_padding1 = r.ReadBytes(blockSize);
+                ReadBigDataBlock(stream, m_simpleVars, m_scripts);
+                ReadBigDataBlock(stream, m_playerPeds);
+                ReadBigDataBlock(stream, m_garages);
+                ReadBigDataBlock(stream, m_vehicles);
+                ReadBigDataBlock(stream, m_objects);
+                ReadBigDataBlock(stream, m_pathFind);
+                ReadBigDataBlock(stream, m_cranes);
+                ReadBigDataBlock(stream, m_pickups);
+                ReadBigDataBlock(stream, m_phoneInfo);
+                ReadBigDataBlock(stream, m_restarts);
+                ReadBigDataBlock(stream, m_radar);
+                ReadBigDataBlock(stream, m_zones);
+                ReadBigDataBlock(stream, m_gangs);
+                ReadBigDataBlock(stream, m_carGenerators);
+                ReadBigDataBlock(stream, m_particles);
+                ReadBigDataBlock(stream, m_audioScriptObjects);
+                ReadBigDataBlock(stream, m_playerInfo);
+                ReadBigDataBlock(stream, m_stats);
+                ReadBigDataBlock(stream, m_streaming);
+                ReadBigDataBlock(stream, m_pedTypes);
+                ReadDataBlock(stream, m_padding0);
+                ReadDataBlock(stream, m_padding1);
             }
 
             return stream.Position - start;
@@ -146,99 +49,28 @@ namespace WHampson.Gta3CarGenEditor.Models
         {
             long start = stream.Position;
             using (BinaryWriter w = new BinaryWriter(stream, Encoding.Default, true)) {
-                // Write SimpleVars and Scripts
-                w.Write(m_simpleVars.Length + m_scripts.Length + 4);
-                w.Write(m_simpleVars);
-                w.Write(m_scripts.Length);
-                w.Write(m_scripts);
-
-                // Write PlayerPeds
-                w.Write(m_playerPeds.Length);
-                w.Write(m_playerPeds);
-
-                // Write Garages
-                w.Write(m_garages.Length);
-                w.Write(m_garages);
-
-                // Write Vehicles
-                w.Write(m_vehicles.Length);
-                w.Write(m_vehicles);
-
-                // Write Objects
-                w.Write(m_objects.Length);
-                w.Write(m_objects);
-
-                // Write PathFind
-                w.Write(m_pathFind.Length);
-                w.Write(m_pathFind);
-
-                // Write Cranes
-                w.Write(m_cranes.Length);
-                w.Write(m_cranes);
-
-                // Write Pickups
-                w.Write(m_pickups.Length);
-                w.Write(m_pickups);
-
-                // Write PhoneInfo
-                w.Write(m_phoneInfo.Length);
-                w.Write(m_phoneInfo);
-
-                // Write Restarts
-                w.Write(m_restarts.Length);
-                w.Write(m_restarts);
-
-                // Write Radar
-                w.Write(m_radar.Length);
-                w.Write(m_radar);
-
-                // Write Zones
-                w.Write(m_zones.Length);
-                w.Write(m_zones);
-
-                // Write Gangs
-                w.Write(m_gangs.Length);
-                w.Write(m_gangs);
-
-                // Write CarGenerators
-                byte[] carGenerators = Serialize(m_carGenerators);
-                w.Write(carGenerators.Length + 4);
-                w.Write(carGenerators.Length);
-                w.Write(carGenerators);
-
-                // Write Particles
-                w.Write(m_particles.Length);
-                w.Write(m_particles);
-
-                // Write AudioScriptObjects
-                w.Write(m_audioScriptObjects.Length);
-                w.Write(m_audioScriptObjects);
-
-                // Write PlayerInfo
-                w.Write(m_playerInfo.Length);
-                w.Write(m_playerInfo);
-
-                // Write Stats
-                w.Write(m_stats.Length);
-                w.Write(m_stats);
-
-                // Write Streaming
-                w.Write(m_streaming.Length);
-                w.Write(m_streaming);
-
-                // Write PedTypes
-                w.Write(m_pedTypes.Length);
-                w.Write(m_pedTypes);
-
-                // Write Padding0
-                w.Write(m_padding0.Length);
-                w.Write(m_padding0);
-
-                // Write Padding1
-                w.Write(m_padding1.Length);
-                w.Write(m_padding1);
-
-                // Write checksum
+                WriteBigDataBlock(stream, m_simpleVars, m_scripts);
+                WriteBigDataBlock(stream, m_playerPeds);
+                WriteBigDataBlock(stream, m_garages);
+                WriteBigDataBlock(stream, m_vehicles);
+                WriteBigDataBlock(stream, m_objects);
+                WriteBigDataBlock(stream, m_pathFind);
+                WriteBigDataBlock(stream, m_cranes);
+                WriteBigDataBlock(stream, m_pickups);
+                WriteBigDataBlock(stream, m_phoneInfo);
+                WriteBigDataBlock(stream, m_restarts);
+                WriteBigDataBlock(stream, m_radar);
+                WriteBigDataBlock(stream, m_zones);
+                WriteBigDataBlock(stream, m_gangs);
+                WriteBigDataBlock(stream, m_carGenerators);
+                WriteBigDataBlock(stream, m_particles);
+                WriteBigDataBlock(stream, m_audioScriptObjects);
+                WriteBigDataBlock(stream, m_playerInfo);
+                WriteBigDataBlock(stream, m_stats);
+                WriteBigDataBlock(stream, m_streaming);
+                WriteBigDataBlock(stream, m_pedTypes);
+                WriteDataBlock(stream, m_padding0);
+                WriteDataBlock(stream, m_padding1);
                 w.Write(GetChecksum(stream));
             }
 
