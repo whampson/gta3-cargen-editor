d70f5facf87d0aaa38c70cff048f01cab0726df7
Wes Hampson <thehambone93@gmail.com>
2018-08-20 00:55:21 -0500

    Set MainWindow and show

--- Gta3CarGenEditor/App.xaml.cs
+++ Gta3CarGenEditor/App.xaml.cs
@@ -1,12 +1,4 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.IO;
-using System.Windows;
-using WHampson.Gta3CarGenEditor.Helpers;
-using WHampson.Gta3CarGenEditor.Models;
+﻿using System.Windows;
 
 namespace WHampson.Gta3CarGenEditor
 {
@@ -17,21 +9,8 @@ namespace WHampson.Gta3CarGenEditor
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            //MainWindow = new MainWindow(new MainViewModel());
-            //MainWindow.Show();
-
-            OpenFileDialog dialog = new OpenFileDialog();
-            if (dialog.ShowDialog() != true) {
-                goto exit;
-            }
-
-            SaveDataFile saveData = SaveDataFile.Load(dialog.FileName);
-
-            byte[] newData = SerializableObject.Serialize(saveData);
-            File.WriteAllBytes(dialog.FileName + ".dmp", newData);
-
-        exit:
-            Application.Current.Shutdown();
+            MainWindow = new MainWindow();
+            MainWindow.Show();
         }
     }
 }
