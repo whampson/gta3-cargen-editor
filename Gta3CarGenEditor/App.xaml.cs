27fcae4a805a8186c8dbbdec2f4756083adc6487
Wes Hampson <thehambone93@gmail.com>
2018-08-23 01:29:11 -0500

    Implement uncaught exception handler

--- Gta3CarGenEditor/App.xaml.cs
+++ Gta3CarGenEditor/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
+using WHampson.Gta3CarGenEditor.Helpers;
 using WHampson.Gta3CarGenEditor.Views;
 
 namespace WHampson.Gta3CarGenEditor
@@ -8,12 +10,27 @@ namespace WHampson.Gta3CarGenEditor
     /// </summary>
     public partial class App : Application
     {
+        const int FatalErrorExitCode = 127;
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            // TODO: uncaught exception handler
-
             MainWindow = new MainWindow();
             MainWindow.Show();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+
+            MessageBoxEx.Show(
+                MainWindow,
+                string.Format("A fatal exception has occurred. The application will be terminated.\n\n{0}: {1}",
+                    e.Exception.GetType().Name, e.Exception.Message),
+                "Unhandled Exception",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+
+            Current.Shutdown(FatalErrorExitCode);
         }
     }
 }
