4ddca63da8704155f3739cd500a87512638f66c9
Wes Hampson <thehambone93@gmail.com>
2018-08-22 22:56:19 -0500

    Add MetadataViewModel

---
+++ Gta3CarGenEditor/ViewModels/MetadataViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+using WHampson.Gta3CarGenEditor.Helpers;
+using WHampson.Gta3CarGenEditor.Models;
+
+namespace WHampson.Gta3CarGenEditor.ViewModels
+{
+    public class MetadataViewModel : ObservableObject
+    {
+        private CarGeneratorsInfo m_metadata;
+
+        public MetadataViewModel(CarGeneratorsInfo metadata)
+        {
+            m_metadata = metadata;
+        }
+
+        public CarGeneratorsInfo Metadata
+        {
+            get { return m_metadata; }
+            set { m_metadata = value; OnPropertyChanged(); }
+        }
+
+        public ICommand OkCommand
+        {
+            get { return new RelayCommand(() => OnDialogCloseRequested(new DialogCloseEventArgs(true))); }
+        }
+
+        public ICommand CancelCommand
+        {
+            get { return new RelayCommand(() => OnDialogCloseRequested(new DialogCloseEventArgs(false))); }
+        }
+
+        public event EventHandler<DialogCloseEventArgs> DialogCloseRequested;
+        protected void OnDialogCloseRequested(DialogCloseEventArgs e)
+        {
+            DialogCloseRequested?.Invoke(this, e);
+        }
+    }
+}
