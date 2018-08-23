fc8a3a96e293f78225d22edc1934dfaa519345e9
Wes Hampson <thehambone93@gmail.com>
2018-08-22 22:55:26 -0500

    Add DialogCloseEventArgs and MetadatDialogEventArgs

---
+++ Gta3CarGenEditor/Helpers/DialogCloseEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WHampson.Gta3CarGenEditor.Helpers
+{
+    /// <summary>
+    /// Parameters for closing a dialog window from an event.
+    /// </summary>
+    public class DialogCloseEventArgs : EventArgs
+    {
+        public DialogCloseEventArgs(bool? dialogResult = null)
+        {
+            DialogResult = dialogResult;
+        }
+
+        public bool? DialogResult
+        {
+            get;
+        }
+    }
+}
---
+++ Gta3CarGenEditor/Helpers/MetadataDialogEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WHampson.Gta3CarGenEditor.Models;
+
+namespace WHampson.Gta3CarGenEditor.Helpers
+{
+    /// <summary>
+    /// Parameters for opening a MetadataWindow.
+    /// </summary>
+    public class MetadataDialogEventArgs : EventArgs
+    {
+        public MetadataDialogEventArgs(CarGeneratorsInfo metadata)
+        {
+            Metadata = metadata;
+        }
+
+        public CarGeneratorsInfo Metadata
+        {
+            get;
+        }
+    }
+}
