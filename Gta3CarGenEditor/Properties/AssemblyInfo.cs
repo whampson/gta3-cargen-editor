795f9d09f3cf0e2e3f23a905d0e6b4b817f04946
Wes Hampson <thehambone93@gmail.com>
2018-08-23 00:41:03 -0500

    Build 0.2.0.357

--- Gta3CarGenEditor/Properties/AssemblyInfo.cs
+++ Gta3CarGenEditor/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly   // generic resource dictionary 
 )]
 
-[assembly: AssemblyVersion("0.2.0.339")]
-[assembly: AssemblyFileVersion("0.2.0.339")]
+[assembly: AssemblyVersion("0.2.0.357")]
+[assembly: AssemblyFileVersion("0.2.0.357")]
 [assembly: AssemblyInformationalVersion("0.2.0")]
