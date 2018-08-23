ec8e987e8f2feb0adaf904f5e273f792bc235570
Wes Hampson <thehambone93@gmail.com>
2018-08-22 23:01:00 -0500

    Add TODOs

--- Gta3CarGenEditor/App.xaml.cs
+++ Gta3CarGenEditor/App.xaml.cs
@@ -9,6 +9,9 @@ namespace WHampson.Gta3CarGenEditor
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            // TODO: uncaught exception handler
+            // TODO: handle I/O exceptions with dialog
+
             MainWindow = new MainWindow();
             MainWindow.Show();
         }
