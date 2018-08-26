6b3cc70718745a8f230b39b9b396b2feb447f391
Wes Hampson <thehambone93@gmail.com>
2018-08-26 13:28:23 -0500

    Pre-release vresion 1.0.0-rc1 (build 422)

--- Gta3CarGenEditor/Properties/AssemblyInfo.cs
+++ Gta3CarGenEditor/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly   // generic resource dictionary 
 )]
 
-[assembly: AssemblyVersion("0.6.0.421")]
-[assembly: AssemblyFileVersion("0.6.0.421")]
-[assembly: AssemblyInformationalVersion("0.6.0")]
+[assembly: AssemblyVersion("1.0.0.422")]
+[assembly: AssemblyFileVersion("1.0.0.422")]
+[assembly: AssemblyInformationalVersion("1.0.0-rc1")]
