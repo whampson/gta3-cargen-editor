2cb3cfc073e541164a3d47e6f1e6bfd021d2851f
Wes Hampson <thehambone93@gmail.com>
2018-08-28 21:18:45 -0500

    Add AboutWindow and view model

---
+++ Gta3CarGenEditor/ViewModels/AboutViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using WHampson.Gta3CarGenEditor.Events;
+using WHampson.Gta3CarGenEditor.Helpers;
+using WHampson.Gta3CarGenEditor.Resources;
+
+namespace WHampson.Gta3CarGenEditor.ViewModels
+{
+    public class AboutViewModel : ObservableObject
+    {
+        public string AppVersion
+        {
+            get {
+                return GetAppVersionString();
+            }
+        }
+
+        private string GetAppVersionString()
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            if (asm == null) {
+                return Strings.TextVersionError;
+            }
+
+            FileVersionInfo vInfo = FileVersionInfo.GetVersionInfo(asm.Location);
+            return string.Format(Strings.TextVersionFormat,
+                vInfo.ProductVersion, vInfo.FilePrivatePart);
+        }
+
+        public ICommand DonateCommand
+        {
+            get {
+                return new RelayCommand(
+                    () => OnNavigationRequested(
+                        new RequestNavigateEventArgs(new Uri(Strings.UrlDonate, UriKind.Absolute), null)));
+            }
+        }
+
+        public ICommand CloseCommand
+        {
+            get { return new RelayCommand(() => OnDialogCloseRequested(new DialogCloseEventArgs(false))); }
+        }
+
+        public event EventHandler<RequestNavigateEventArgs> NavigationRequested;
+        protected void OnNavigationRequested(RequestNavigateEventArgs e)
+        {
+            NavigationRequested?.Invoke(this, e);
+        }
+
+        public event EventHandler<DialogCloseEventArgs> DialogCloseRequested;
+        protected void OnDialogCloseRequested(DialogCloseEventArgs e)
+        {
+            DialogCloseRequested?.Invoke(this, e);
+        }
+    }
+}
---
+++ Gta3CarGenEditor/Views/AboutWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WHampson.Gta3CarGenEditor.ViewModels;
+
+namespace WHampson.Gta3CarGenEditor.Views
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+
+            ViewModel.DialogCloseRequested += ViewModel_DialogCloseRequested;
+            ViewModel.NavigationRequested += Hyperlink_RequestNavigate;
+        }
+
+        public AboutViewModel ViewModel
+        {
+            get { return (AboutViewModel) DataContext; }
+            set { DataContext = value; }
+        }
+
+        private void ViewModel_DialogCloseRequested(object sender, Events.DialogCloseEventArgs e)
+        {
+            DialogResult = e.DialogResult;
+            Close();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            if (e.Uri != null) {
+                Process.Start(e.Uri.ToString());
+            }
+        }
+    }
+}
