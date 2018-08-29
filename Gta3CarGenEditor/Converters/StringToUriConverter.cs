51f521a82ec9b88d11ab16c58fb69e27caa7d3d2
Wes Hampson <thehambone93@gmail.com>
2018-08-28 21:17:34 -0500

    Add StringToUriConverter

---
+++ Gta3CarGenEditor/Converters/StringToUriConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WHampson.Gta3CarGenEditor.Converters
+{
+    /// <summary>
+    /// Converts a <see cref="String"/> into a <see cref="Uri"/> and back.
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(Uri))]
+    public class StringToUriConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string s = value as string;
+            return string.IsNullOrEmpty(s) ? null : new Uri(s, UriKind.Absolute);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Uri u = value as Uri;
+            return (u == null) ? string.Empty : u.ToString();
+        }
+    }
+}
